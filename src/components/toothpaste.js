639af12abc9375a5e733e54d020f1736fbb33461
dotoriPocket <monparis12@naver.com>
2023-03-16 09:42:32 +0900

    add line move

--- src/components/toothpaste.js
+++ src/components/toothpaste.js
@@ -1,17 +1,40 @@
 import toothpasteSrc from "../assets/shape_toothpaste.png";
 import ToothpasteLine from "./toothpasteLine";
 import "./toothpaste.css";
+import "./toothpasteLine.css";
 
 function ToothPaste() {
-  const toothpasteMerge = document.createElement("div");
+  const toothpasteContainer = document.createElement("div");
   const toothpasteImg = document.createElement("img");
+  const toothpasteLine = ToothpasteLine();
 
   toothpasteImg.setAttribute("src", toothpasteSrc);
   toothpasteImg.setAttribute("class", "toothpaste_style");
-  toothpasteMerge.appendChild(toothpasteImg);
-  toothpasteMerge.appendChild(ToothpasteLine());
+  toothpasteContainer.appendChild(toothpasteImg);
+  toothpasteContainer.appendChild(toothpasteLine);
 
-  return toothpasteMerge;
+  let interval = -1;
+  let left = -587;
+
+  const MouseDown = function mouseDownHandler() {
+    interval = setInterval(() => {
+      left += 20;
+      if (left > 430) {
+        toothpasteLine.style.left = `${430}px`;
+      } else {
+        toothpasteLine.style.left = `${left}px`;
+      }
+    }, 50);
+  };
+
+  const MouseUp = function mouseUpHandler() {
+    clearInterval(interval);
+  };
+
+  toothpasteImg.addEventListener("mousedown", MouseDown);
+  toothpasteImg.addEventListener("mouseup", MouseUp);
+
+  return toothpasteContainer;
 }
 
 export default ToothPaste;
